88c999bbfa5b68490c4961eaf2d984260510e883
Martin Jansche <mjansche@google.com>
2016-05-05 17:07:45 +0100

    G2P models for Afrikaans, rebuilt at tip of https://github.com/googlei18n/language-resources/tree/master/af

--- usr_local/src/g2p/g2p.sh
+++ usr_local/src/g2p/g2p.sh
@@ -3,9 +3,23 @@
 set -o errexit
 set -o nounset
 
-modeldir="$(dirname $0)"
+if [ "$#" -eq 0 ]; then
+  echo "Usage: $0 model_directory [word_list]" >&2
+  exit 2
+fi
 
-exec g2p-lookup \
+dir="$(dirname $0)"
+model="$1"
+shift
+
+if [ ! -d "$dir/models/$model" ]; then
+  echo "$model is not a directory under $dir/models/" >&2
+  exit 2
+fi
+
+modeldir="$dir/models/$model"
+
+exec "$dir/runtime/g2p-lookup" \
   --bytes_to_graphones="$modeldir/bytes_to_graphones.fst" \
   --graphone_model="$modeldir/graphone_model.fst" \
   --phonemes_to_graphones="$modeldir/phonemes_to_graphones.fst" \
